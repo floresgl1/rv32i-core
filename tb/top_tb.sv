5affcdcf2025a3a10e2719dd268b43bb98d5f28c
floresgl1 <floresgl907@gmail.com>
2026-09-05 12:35:44 -0700

    Add initial RTL and testbench files with Makefile for simulation

---
+++ tb/top_tb.sv
@@ -0,0 +1,19 @@
+module top_tb;
+
+
+    logic clk, rst_n;
+
+    always #5 clk = ~clk;
+    top u_top (
+        .clk(clk),
+        .rst_n(rst_n)
+    );
+    initial begin
+    clk = 0;
+    rst_n = 0;
+    #20 rst_n = 1;
+    repeat(10) @(posedge clk);
+    $display("PASS"); $finish;
+    end
+
+endmodule
